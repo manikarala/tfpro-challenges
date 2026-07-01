b4ff662a185356225cf84ee503dab88f2b69f91a
Veera Mani Kanta Karala <manikanta530.cse@gmail.com>
2026-07-01 23:20:40 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ resource "random_integer" "priority" {
   min = 1
   max = 50000
 }
-variable variable_key{}
+variable variable_key_1{}
 output variable_key {
-  value = var.variable_key
+  value = var.variable_key_1
 }
