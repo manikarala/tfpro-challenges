d74f065e03001a1774e5ebaa04e3439cc6c05efd
Veera Mani Kanta Karala <manikanta530.cse@gmail.com>
2026-07-01 23:28:47 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,7 +2,22 @@ resource "random_integer" "priority" {
   min = 1
   max = 50000
 }
-variable variable_key_1{}
+
+
+variable variable_key{}
 output variable_key {
+  value = var.variable_key
+}
+
+
+variable variable_key_1{}
+output variable_key_1 {
   value = var.variable_key_1
+}
+
+
+
+variable variable_key_project_level{}
+output variable_key_project_level {
+  value = var.variable_key_project_level
 }
